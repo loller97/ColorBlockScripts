02914a7eb1a094f64aebe974d596882b76464b25
Simone Dosio <98324487+loller97@users.noreply.github.com>
2026-01-27 11:24:13 +0100

    Aggiunto il ModuleScript

---
+++ ModuleScript.lua
@@ -0,0 +1,18 @@
+local RoundTimer = {}
+
+RoundTimer.WaitPlayersTime = true
+
+RoundTimer.RestTime = 5
+
+RoundTimer.PreparationTime = 5
+
+RoundTimer.GameTime = false
+
+RoundTimer.WinnerTime = 5
+
+RoundTimer.RoundTime = 5
+
+RoundTimer.VoteTime = 15
+RoundTimer.VoteStart = false
+
+return RoundTimer
